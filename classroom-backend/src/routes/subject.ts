483eacd07bc41f942354f17ec1e5e922b8e88873
SaadHussein <sa3dhusseein@gmail.com>
2026-02-19 21:43:28 +0200

    fix bugs after review with codeRabbit

--- classroom-backend/src/routes/subject.ts
+++ classroom-backend/src/routes/subject.ts
@@ -1,4 +1,13 @@
-import { and, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
+import {
+  and,
+  count,
+  desc,
+  eq,
+  getTableColumns,
+  ilike,
+  or,
+  sql,
+} from "drizzle-orm";
 import express from "express";
 import { departments, subjects } from "../db/schema";
 import { db } from "../db";
@@ -41,12 +50,12 @@ router.get("/", async (req, res) => {
       filterConditions.length > 0 ? and(...filterConditions) : undefined;
 
     const countResult = await db
-      .select({ count: sql<number>`count(*)` })
+      .select({ count: count() })
       .from(subjects)
       .leftJoin(departments, eq(subjects.departmentId, departments.id))
       .where(whereClause);
 
-    const totalount = countResult[0]?.count ?? 0;
+    const totalCount = countResult[0]?.count ?? 0;
 
     const subjectsList = await db
       .select({
@@ -57,15 +66,16 @@ router.get("/", async (req, res) => {
       .leftJoin(departments, eq(subjects.departmentId, departments.id))
       .where(whereClause)
       .limit(limitPerPage)
-      .offset(offset);
+      .offset(offset)
+      .orderBy(desc(subjects.createdAt));
 
     res.status(200).json({
       data: subjectsList,
       pagination: {
-        total: totalount,
+        total: totalCount,
         page: currentPage,
         limit: limitPerPage,
-        totalPages: Math.ceil(totalount / limitPerPage),
+        totalPages: Math.ceil(totalCount / limitPerPage),
       },
     });
   } catch (err) {
